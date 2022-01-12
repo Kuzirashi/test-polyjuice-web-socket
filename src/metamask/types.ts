cd1133377b95844424fa37e5bc066f0f3f9294f1
Daniel Kmak <neverkuzi@gmail.com>
2022-01-12 17:15:22 +0100

    Deploy

--- src/metamask/types.ts
+++
@@ -1,8 +0,0 @@
-export enum MetamaskActions {
-    requestAccounts = 'eth_requestAccounts',
-    requestWallet = 'wallet_requestPermissions',
-    watchAsset = 'wallet_watchAsset',
-    switchNetwork = 'wallet_switchEthereumChain',
-    accountsChanged = 'accountsChanged',
-  }
-  
